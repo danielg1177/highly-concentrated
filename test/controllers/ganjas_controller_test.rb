23dd1acb5303a13a08bff53fc254f2168bf06edb
daniel gordon <danielgordon19972@gmail.com>
2021-05-02 10:51:45 +0300

    added controllers

---
+++ test/controllers/ganjas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GanjasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/purchase_requests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PurchaseRequestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
