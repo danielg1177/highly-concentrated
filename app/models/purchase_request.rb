58ead455c09350f59af1bc8f9da15e36fcfdb5f1
hugo gurney <hugogurney5@gmail.com>
2021-05-02 14:17:32 +0200

    validations updated

--- app/models/purchase_request.rb
+++ app/models/purchase_request.rb
@@ -3,5 +3,7 @@ class PurchaseRequest < ApplicationRecord
   belongs_to :ganja
 
   validates :pickup_time, presence: true
-  validates :status, inclusion: { in: %w[Accepted accepted] }
+  validates :status, inclusion: { in: %w[Accepted accepted
+                                         Pending pending
+                                         Declined declined] }
 end
