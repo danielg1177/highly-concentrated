f337dad54e4002e3a34000dd13e214006bddaa5a
Moshe Maman <moshemaman20@gmail.com>
2021-05-03 15:24:37 +0300

    mapbox highly conentrated

--- app/models/ganja.rb
+++ app/models/ganja.rb
@@ -8,4 +8,7 @@ class Ganja < ApplicationRecord
   validates :unit_price, presence: true
   validates :pickup_local, presence: true
   validates :variety, inclusion: { in: %w[edible flower] }
+
+  geocoded_by :pickup_local
+  after_validation :geocode, if: :will_save_change_to_pickup_local?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_153735) do
+ActiveRecord::Schema.define(version: 2021_05_03_103624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_153735) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_ganjas_on_user_id"
   end
 
