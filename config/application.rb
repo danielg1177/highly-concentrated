80987be95fa96f36ff176d99955d61130f75914e
daniel gordon <danielgordon19972@gmail.com>
2021-05-04 15:04:07 +0300

    changed to date time

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module HighlyConcentrated
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
