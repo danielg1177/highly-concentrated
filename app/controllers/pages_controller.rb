435240429d1e33ead50a98985daec8cf4882d68f
hugo gurney <hugogurney5@gmail.com>
2021-05-04 16:32:41 +0200

    fixing datetime

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[home about_us
+                                                   contact_us faq]
   def home
+  end
+
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def faq
   end
 end
