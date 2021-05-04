81a89c142a5afc3d90455b7ec2d6eebc260cba0a
Moshe Maman <moshemaman20@gmail.com>
2021-05-04 14:46:26 +0300

    mapbox

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
    def home
-     @ganjas = policy_scope(Ganja)
+    @ganjas = policy_scope(Ganja)
     @markers = @ganjas.geocoded.map do |ganja|
       {
         lat: ganja.latitude,
         lng: ganja.longitude
       }
     end
- end
+  end
 end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,11 +12,20 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers)
     markers.forEach((marker) => {
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contian';
+      element.style.width = '45px';
+      element.style.height = '45px';
+
       new mapboxgl.Marker()
       .setLnglat([marker.lng, marker.lat])
       .addTo(map);
     });
     fitMapToMarkers(map,markers)
+    map.addControl(new Mapbox({accessToken: mapboxgl.accessToken,mapboxgl: mapboxgl}));
   }
 };
 
