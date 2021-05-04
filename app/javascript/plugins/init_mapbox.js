aecb6add5b0324422b777133eee7416cc2d1d4dd
Moshe Maman <moshemaman20@gmail.com>
2021-05-04 17:07:34 +0300

    mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -10,22 +16,23 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-    const markers = JSON.parse(mapElement.dataset.markers)
-    markers.forEach((marker) => {
-
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    // Create a HTML element for your custom marker
       const element = document.createElement('div');
       element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contian';
-      element.style.width = '45px';
-      element.style.height = '45px';
-
-      new mapboxgl.Marker()
-      .setLnglat([marker.lng, marker.lat])
+      element.style.backgroundImage = `url('https://e7.pngegg.com/pngimages/499/98/png-clipart-cannabis-weed-golf-club-readygolf-marijuana-pot-leaf-ball-marker-hat-clip-bud-cannabis-leaf-black-backpack-leaf-hemp-family.png')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '20px';
+      element.style.height = '10px';
+    // Pass the element as an argument to the new marker
+    new mapboxgl.Marker(element)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
       .addTo(map);
-    });
+  });
     fitMapToMarkers(map,markers)
-    map.addControl(new Mapbox({accessToken: mapboxgl.accessToken,mapboxgl: mapboxgl}));
   }
 };
 
